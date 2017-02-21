a047d90aaa55f1ff22ef2445db1498e67e8a2bec
Edison Osorio <edisonosorio@nusoft.com.co>
2017-02-21 12:23:09 -0500

    Avances para inicio a registro de base de datos

---
+++ admin.php
@@ -0,0 +1,208 @@
+<!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Movich Salones</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a href="index.html" class="logo">
+									<span class="symbol"><img src="images/logo2.png" alt="" /></span><span class="title">Movich Salones</span>
+								</a>
+
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<!-- <li><a href="index.html">Inicio</a></li> -->
+							<li><a href="index.html">Salones</a></li>
+							<li><a href="restaurante.html">Restaurante</a></li>
+							<li><a href="spa.html">Spa</a></li>
+							<li><a href="jardines.html">Jardines</a></li>
+							<li><a href="elements.html">Elementos</a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<header>
+								<h1>Disponibilidad de los salones en el Movich Llanogrande</h1>
+								<p>El Hotel Movich Las Lomas cuenta con una excelente infraestructura de 10 salones para conferencias y convenciones con capacidades de 5 y 230 personas. Cuenta también con una zona de piscina para cocteles y reuniones, amplios jardines y se cuenta con todas las ayudas audiovisuales.</p>
+								<p>Disponibilidad: <input type="date" name="fecha"></p>
+							</header>
+							<section class="tiles">
+								<article class="style1">
+									<span class="image">
+										<img src="images/salon01.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 01</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 12 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style2">
+									<span class="image">
+										<img src="images/salon02.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 02</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 18 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style3">
+									<span class="image">
+										<img src="images/salon03.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 03</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 30 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style4">
+									<span class="image">
+										<img src="images/salon04.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 04</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 12 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style5">
+									<span class="image">
+										<img src="images/salon05.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 05</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 15 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style6">
+									<span class="image">
+										<img src="images/salon06.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 06</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 30 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style2">
+									<span class="image">
+										<img src="images/salon07.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 07</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 12 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style3">
+									<span class="image">
+										<img src="images/salon08.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 08</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 15 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style1">
+									<span class="image">
+										<img src="images/salon08.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Salón 09</h2>
+										<div class="content">
+											<p id='estado'>Salón dispuesto para 30 personas con los implementos necesarios para una reunión.</p>
+										</div>
+									</a>
+							</section>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<h2>Haz tu reserva:</h2>
+								<form method="post" action="#">
+									<div class="field half first">
+										<input type="text" name="name" id="name" placeholder="Name" />
+									</div>
+									<div class="field half">
+										<input type="email" name="email" id="email" placeholder="Email" />
+									</div>
+									<div class="field">
+										<textarea name="message" id="message" placeholder="Message"></textarea>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send" class="special" /></li>
+									</ul>
+								</form>
+							</section>
+							<section>
+								<h2>Buscanos en:</h2>
+								<ul class="icons">
+									<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy; AlDía. All rights reserved</li><li>Design: <a href="www.edisonosorioj.com">Edison Osorio</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
